8ea2106ecf6642c8a1336b1a5eae89460ea14837
vnemana <venkat.nemana@gmail.com>
2018-05-21 21:38:32 -0700

    Refactoring search results

--- src/main/java/org/gradle/utilities/Search13FResultsPage.java
+++ src/main/java/org/gradle/utilities/Search13FResultsPage.java
@@ -4,7 +4,6 @@ import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.*;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.util.List;
 
 public class Search13FResultsPage {
@@ -17,29 +16,11 @@ public class Search13FResultsPage {
 
     public String get13FFilingLink () {
         try (final WebClient webClient = new WebClient()) {
-
             final HtmlPage search13fResultsPage = webClient.getPage(searchURL);
-//            List<DomElement> domElements = search13fResultsPage.getByXPath("/html/body/div[4]/div[4]/table/tbody/tr[2]/td[2]/a/@href");
-//            DomElement dom = domElements.get(0);
-//            System.out.println(dom.getTextContent());
-
-            HtmlTable results13fTable = search13fResultsPage.getBody().getOneHtmlElementByAttribute(
-                    "table", "summary", "Results");
-            int numRows = results13fTable.getRowCount();
-            if (numRows > 1) {
-                List<HtmlAnchor> htmlAnchors13f = search13fResultsPage.getAnchors();
-                for (int rowNum = 1; rowNum < 2; rowNum++) {
-                    HtmlTableRow row13F = results13fTable.getRow(rowNum);
-                    HtmlTableCell htmlTableCell = row13F.getCell(2);
-                    String accountString = htmlTableCell.asText();
-                    String accountStringArray[] = accountString.split(":");
-                    String accountArray[] = accountStringArray[1].split(" ");
-                    for (HtmlAnchor ha13F : htmlAnchors13f) {
-                        if (ha13F.getHrefAttribute().contains(accountArray[1])) {
-                            return searchSite + ha13F.getHrefAttribute();
-                        }
-                    }
-                }
+            List<DomNode> domNodes = search13fResultsPage.getByXPath(
+                    "/html/body/div[4]/div[4]/table/tbody/tr[2]/td[2]/a/@href");
+            if (domNodes.size() == 1) {
+                return searchSite + domNodes.get(0).getTextContent();
             }
         } catch (IOException e) {
             e.printStackTrace();
